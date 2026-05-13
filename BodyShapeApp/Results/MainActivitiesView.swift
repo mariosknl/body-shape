327a6804b49e815db9f35909e208dfa58c8f7eed
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-13 15:17:18 +0300

    Completing Result View

--- BodyShapeApp/Results/MainActivitiesView.swift
+++ BodyShapeApp/Results/MainActivitiesView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct MainActivitiesView: View {
+    let activities: [Activity]
     var body: some View {
-        Text("Main Activities")
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(activities) { activity in
+                    CurrentActiviesView(activity: activity)
+                }
+            }.padding()
+        }
     }
 }
 
+struct Activity: Identifiable {
+    let id = UUID()
+    let dim: CGFloat
+    let activityName: String
+    let activityImage: String
+}
+
 #Preview {
-    MainActivitiesView()
+    MainActivitiesView(
+        activities: [
+            .init(
+                dim: 150,
+                activityName: "Running",
+                activityImage: "figure.run"
+            ),
+            .init(
+                dim: 150,
+                activityName: "Biking",
+                activityImage: "figure.outdoor.cycle"
+            ),
+            .init(
+                dim: 150,
+                activityName: "Climbing",
+                activityImage: "figure.climbing"
+            ),
+            .init(
+                dim: 150,
+                activityName: "Skating",
+                activityImage: "figure.skating"
+            )
+        ]
+    )
 }
--- BodyShapeApp/Results/ResultView.swift
+++ BodyShapeApp/Results/ResultView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ResultView: View {
     let selectedWeightUnits = "lb"
     var body: some View {
-        VStack(spacing: 30) {
+        VStack {
             VStack(spacing: 0) {
                 ResultsTopRow()
                     .padding()
@@ -48,18 +48,41 @@ struct ResultView: View {
                 )
             }
             
-            MainActivitiesView()
-            
-            HStack {
-                Text("Your plan for December, 2022")
+            VStack(spacing: 0) {
+                MainActivitiesView(activities: [
+                    .init(
+                        dim: 150,
+                        activityName: "Running",
+                        activityImage: "figure.run"
+                    ),
+                    .init(
+                        dim: 150,
+                        activityName: "Biking",
+                        activityImage: "figure.outdoor.cycle"
+                    ),
+                    .init(
+                        dim: 150,
+                        activityName: "Climbing",
+                        activityImage: "figure.climbing"
+                    ),
+                    .init(
+                        dim: 150,
+                        activityName: "Skating",
+                        activityImage: "figure.skating"
+                    )
+                ])
                 
-                Spacer()
-                
-                Text("1.8")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                Text(selectedWeightUnits)
-            }.padding(.horizontal)
+                HStack {
+                    Text("Your plan for December, 2022")
+                    
+                    Spacer()
+                    
+                    Text("1.8")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Text(selectedWeightUnits)
+                }.padding(.horizontal)
+            }
             
             HeartRateRow()
             
