b9813b5843785bff5563d53d4c2bb41511cf50c7
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-13 14:37:55 +0300

    Calendar Bar view

---
+++ BodyShapeApp/Results/Calendar Bar View/CalendarBarView.swift
@@ -0,0 +1,106 @@
+//
+//  CalendarBarView.swift
+//  BodyShapeApp
+//
+//  Created by Marios Kanellopoulos on 13/5/26.
+//
+
+import SwiftUI
+
+struct DailyDataModel: Identifiable {
+    let id = UUID()
+    let day: String
+    let percentage: CGFloat
+    let amount: Int
+}
+
+struct CalendarBarView: View {
+    let selectedDay = "Thu"
+    let dailyData: [DailyDataModel]
+    
+    let height: CGFloat
+    let offsetY: CGFloat
+    let selectedWeight: Int
+    let selectedWeightUnits: String = "lb"
+    let selectedDim = 70.0
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Rectangle()
+                    .stroke(
+                        style: StrokeStyle(
+                            lineWidth: 0.2,
+                            lineCap: .square,
+                            dash: [1, 5]
+                        )
+                    )
+                    .frame(height: 1)
+                
+                HStack(
+                    alignment: .bottom
+                ) {
+                    ForEach(dailyData) { data in
+                        VerticalDotBar(
+                            percent: data.percentage,
+                            isSelected: data.day == selectedDay,
+                            height: height,
+                            offsetY: offsetY,
+                            selectedWeight: data.amount
+                        )
+                        .frame(maxWidth: .infinity)
+                    }
+                }
+                .offset(y: 100 / 2 - 13)
+            }
+        }
+        
+        HStack(alignment: .bottom) {
+            ForEach(dailyData) { data in
+                VStack {
+                    Text(data.day)
+                        .frame(maxWidth: .infinity)
+                    
+                    Text("\(data.amount, specifier: "%.2d")")
+                        .frame(maxWidth: .infinity)
+                }
+                .fontWeight(data.day == selectedDay ? .bold : .regular)
+            }
+        }
+    }
+}
+
+#Preview {
+    CalendarBarView(
+        dailyData: [
+            .init(
+                day: "Mon",
+                percentage: 0.6,
+                amount: 1
+            ),
+            .init(
+                day: "Tue",
+                percentage: 0.3,
+                amount: 5
+            ),
+            .init(
+                day: "Web",
+                percentage: 0.5,
+                amount: 8
+            ),
+            .init(
+                day: "Thu",
+                percentage: 1.0,
+                amount: 12
+            ),
+            .init(
+                day: "Fri",
+                percentage: 0.3,
+                amount: 15
+            )
+        ],
+        height: 100,
+        offsetY: -5,
+        selectedWeight: 12
+    )
+}
--- BodyShapeApp/Results/CurrentActiviesView.swift
+++ BodyShapeApp/Results/CurrentActiviesView.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 struct CurrentActiviesView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             ResultsTopRow()
                 .padding()
+            CalendarBarView(
+                dailyData: [
+                    .init(
+                        day: "Mon",
+                        percentage: 0.6,
+                        amount: 1
+                    ),
+                    .init(
+                        day: "Tue",
+                        percentage: 0.3,
+                        amount: 5
+                    ),
+                    .init(
+                        day: "Web",
+                        percentage: 0.5,
+                        amount: 8
+                    ),
+                    .init(
+                        day: "Thu",
+                        percentage: 1.0,
+                        amount: 12
+                    ),
+                    .init(
+                        day: "Fri",
+                        percentage: 0.3,
+                        amount: 15
+                    )
+                ],
+                height: 100,
+                offsetY: -5,
+                selectedWeight: 12
+            )
             Spacer()
         }
     }
