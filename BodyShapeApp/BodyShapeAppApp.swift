48583be17f537f93fbff6ef1e4a1f78a6771b796
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-13 11:19:39 +0300

    Initial Commit

---
+++ BodyShapeApp/BodyShapeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyShapeAppApp.swift
+//  BodyShapeApp
+//
+//  Created by Marios Kanellopoulos on 13/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BodyShapeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
